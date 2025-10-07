b06e4eb0622be66cf815da821cd39f39547e15a3
HidayahMF <hidayahmfadillah@gmail.com>
2025-10-07 17:45:58 +0700

    frontend-update-home

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex flex-row justify-center w-full">
+      <div className="flex flex-row w-300">
+        <h1 className="font-extrabold text-[28px] mt-3 text-white">
+          Dashboard
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/view/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Dashboard = () => {
+  return (
+    <div className="flex bg-black h-screen">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Dashboard;
