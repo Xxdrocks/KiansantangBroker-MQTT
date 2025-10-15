680d729baaa72fca0107f97e9f667c300a03c485
Xxdrocks <kerjaanpahri@gmail.com>
2025-10-15 22:48:51 +0700

    routing update

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 use App\Http\Controllers\Api\InputEmissionController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DevicesController;
 
