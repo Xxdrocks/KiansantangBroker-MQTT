4f4860bdc95effe82acdc0a05b3ad177d867d585
Xxdrocks <kerjaanpahri@gmail.com>
2025-10-15 15:22:27 +0700

    inputemission-db

--- backend/app/Http/Controllers/Api/InputEmissionController.php
+++ backend/app/Http/Controllers/Api/InputEmissionController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\InputEmission;
 use Illuminate\Http\Request;
 
-class InputEmissionController 
+class InputEmissionController
 {
     public function index()
     {
